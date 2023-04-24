fa6e6699aca67635b83c6eeae57ebe00ee2b00d1
YumNumm <onory0917@gmail.com>
2023-04-24 23:35:18 +0900

    init

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<sunmi_printer_plus/SunmiPrinterPlusPlugin.h>)
+#import <sunmi_printer_plus/SunmiPrinterPlusPlugin.h>
+#else
+@import sunmi_printer_plus;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SunmiPrinterPlusPlugin registerWithRegistrar:[registry registrarForPlugin:@"SunmiPrinterPlusPlugin"]];
+}
+
+@end
